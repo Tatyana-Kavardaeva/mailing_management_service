7ed9465678e0006fce1b21417846697885fc19b0
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-09-20 22:23:12 +0300

    updated main\apps.py

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from main.services import start_mailing
 
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+    verbose_name = 'Рассылки'
+    #
+    # def ready(self):
+    #     start_mailing()
