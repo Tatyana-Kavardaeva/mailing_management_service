7c9fa29f820c26fa14ebd4485d7cb4cbc6cdd1be
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-09-25 00:36:43 +0300

    updated settings.py added LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL

--- config/settings.py
+++ config/settings.py
@@ -131,9 +131,9 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
-#
-# LOGIN_REDIRECT_URL = '/'
-# LOGOUT_REDIRECT_URL = '/'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
